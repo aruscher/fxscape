013c417645d284344ab589c092947c1200d3949e
Andreas Ruscheinski <andreas.ruscheinski@googlemail.com>
2019-03-18 18:56:59 +0100

    initial commit

---
+++ src/main/java/org/aruscher/fxscape/FxScapeApp.java
@@ -0,0 +1,14 @@
+package org.aruscher.fxscape;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FxScapeApp extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+
+
+        primaryStage.show();
+    }
+}
